c71e507f215a451cbbe6cc23025f9edc53e5df79
Anthicius <andrius.makseckas@gmail.com>
2026-01-05 13:13:12 +0200

    Fetching users function commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+const fetchUsers = async () => {
+const response = await fetch("https://jsonplaceholder.typicode.com/users")
+const data = await response.json();
+if(!response.ok) throw new Error("There was an issue fetching movies.")
+return data;
+
+}
+
+function App() {
+  
+  const [users, setUsers] = useState([])
+
+  useEffect(()=>{
+    const load = async () => {
+      try {
+        const data = await fetchUsers() 
+        setUsers(data)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    load();
+    
+  },[])
+  
+  
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <ul>
+      {users.map((user)=>(
+        <li key = {user.id}>
+          <h1>{user.name}</h1>
+          <p>{user.email}</p>
+        </li>
+      ))}
+    </ul>
+
     </>
   )
 }
